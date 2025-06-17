4f3a1b8c119e4a9eb2f8a282978907e0c4138952
mkzopio <mk@zopio.com>
2025-06-17 17:40:35 +0300

    feat : initialize website app with design system, cms, seo, analytics and observability features

---
+++ apps/web/app/[locale]/components/header/language-switcher.tsx
@@ -0,0 +1,66 @@
+'use client';
+
+import { Button } from '@repo/design-system/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@repo/design-system/components/ui/dropdown-menu';
+import { Languages } from 'lucide-react';
+import { useParams, usePathname, useRouter } from 'next/navigation';
+
+const languages = [
+  { label: '🇬🇧 English', value: 'en' },
+  { label: '🇪🇸 Español', value: 'es' },
+  { label: '🇩🇪 Deutsch', value: 'de' },
+  { label: '🇹🇷 Türkçe', value: 'tr' },
+];
+
+export const LanguageSwitcher = () => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const params = useParams();
+
+  const switchLanguage = (locale: string) => {
+    const defaultLocale = 'en';
+    let newPathname = pathname;
+
+    // Case 1: If current locale is default and missing from the URL
+    if (
+      !pathname.startsWith(`/${params.locale}`) &&
+      params.locale === defaultLocale
+    ) {
+      // Add the default locale to the beginning to normalize
+      newPathname = `/${params.locale}${pathname}`;
+    }
+
+    // Replace current locale with the selected one
+    newPathname = newPathname.replace(`/${params.locale}`, `/${locale}`);
+    console.log(newPathname);
+
+    router.push(newPathname);
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="ghost"
+          size="icon"
+          className="shrink-0 text-foreground"
+        >
+          <Languages className="h-[1.2rem] w-[1.2rem]" />
+          <span className="sr-only">Switch language</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        {languages.map(({ label, value }) => (
+          <DropdownMenuItem key={value} onClick={() => switchLanguage(value)}>
+            {label}
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
