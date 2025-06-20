e591858f911ad3651d36687213153fde3e63d157
mkzopio <mk@zopio.com>
2025-06-20 19:01:44 +0300

    fix: issue#11 - When running "pnpm dev", the build fails for some packages due to missing scripts/index.ts file

---
+++ packages/api-key/index.ts
@@ -0,0 +1,3 @@
+// Export all functionality from the lib directory
+export * from './lib/clerk-adapter';
+export * from './lib/validate';
---
+++ packages/api-key/scripts/index.ts
@@ -0,0 +1,5 @@
+// This file is required by the build process
+// It can contain build-time utilities, configurations, or exports
+
+// Re-export all functionality from the main index
+export * from '../index';
