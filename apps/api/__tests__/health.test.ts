cd4dc40b7c8824feb1654faeb77c16910b288578
mkzopio <mk@zopio.com>
2025-06-21 04:43:10 +0300

    fix: no need a plain healt.test anymore

--- apps/api/__tests__/health.test.ts
+++
@@ -1,8 +0,0 @@
-import { expect, test } from 'vitest';
-import { GET } from '../app/health/route';
-
-test('Health Check', async () => {
-  const response = await GET();
-  expect(response.status).toBe(200);
-  expect(await response.text()).toBe('OK');
-});
